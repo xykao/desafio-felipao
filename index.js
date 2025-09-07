f59115f2c0193cde1f038192ebdf2fcb09e30c64
xykao <souza.rf@hotmail.com>
2025-09-07 17:22:09 -0300

    desafio do heroi

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-asdfds
+let jogadorNumero1 = "Gimli filho de Gloin";
+let XP = 17000; 
+let nivel = "";
+
+if (XP >=0 && XP <= 1000){
+nivel = "Ferro"
+ 
+} else if (XP >= 1001 && XP <= 2000){
+nivel = "Bronze"
+
+} else if (XP >= 2001 && XP <= 5000){
+nivel = "Prata"
+
+} else if (XP >= 5001 && XP <= 7000){
+nivel = "Ouro"
+
+} else if (XP >= 7001 && XP <= 8000){
+nivel = "Platina"
+
+} else if (XP >= 8001 && XP <= 9000){
+nivel = "Ascendente" 
+
+} else if (XP >= 9001 && XP <= 10000){
+nivel = "Imortal"
+
+} else if (XP >= 10001){
+nivel = "Radiante"
+}
+
+console.log(`O Herói de nome ${jogadorNumero1} está no nível de ${nivel}!`);
+
+ 
